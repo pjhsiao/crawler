11eb2b296d92d498624ed3151db74c569ae7b091
pjhsiao <pjhsiao99@gmail.com>
2021-11-09 11:37:42 +0800

    improvement performance

--- src/main/java/com/ecommerce/crawler/job/CrawlerJob.java
+++ src/main/java/com/ecommerce/crawler/job/CrawlerJob.java
@@ -36,6 +36,10 @@ public class CrawlerJob {
 
     private boolean momoFirstLoop = true;
 
+    private boolean sinyaFirstLoop = true;
+
+    private boolean coolpcFirstLoop = true;
+
 //    @Scheduled(cron = "0 05 15 * * MON-FRI")
 //    public void rebuild_stock() {
 //    	log.info("Rebuild Cache for daily job");
@@ -45,7 +49,7 @@ public class CrawlerJob {
 //	    	.subscribe();
 //    }
 
-    @Scheduled(initialDelay=888, fixedRate=8888)
+    @Scheduled(initialDelay=10000, fixedRate=13000)
     public void momoJob() {
         log.info("crawler momo for fixed time");
         List<String> resultData = momoCrawlerService.doCrawler();
@@ -54,8 +58,8 @@ public class CrawlerJob {
             resultData.forEach(item->{
                 try {
                     messager.send(String.format("%s\n%s",momoCrawlerService.getStoreTitle(), item));
-                    Thread.sleep(2500l);
                     log.info("has been sent :{}", item);
+                    Thread.sleep(2500l);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -64,25 +68,26 @@ public class CrawlerJob {
         momoFirstLoop =false;
     }
 
-    @Scheduled(initialDelay=9000, fixedRate=30000)
+    @Scheduled(initialDelay=5000, fixedRate=30000)
     public void sinyaJob() {
         log.info("crawler sinya for fixed time");
         List<String> resultData = sinyaCrawlerService.doCrawler();
-        if(!resultData.isEmpty()){
+        if(!resultData.isEmpty() && !sinyaFirstLoop ){
             StringBuffer sb = new StringBuffer(sinyaCrawlerService.getStoreTitle());
             sb.append("\n");
             resultData.forEach(item->{
-                sb.append(item).append("\n\n");
+                sb.append(item).append("\n");
             });
             messager.send(sb.toString());
         }
+        sinyaFirstLoop = false;
     }
 
-    @Scheduled(initialDelay=6000, fixedRate=30000)
+    @Scheduled(initialDelay=1000, fixedRate=30000)
     public void coolpcJob() {
     	log.info("crawler coolpc for fixed time");
         List<String> resultData = coolpcCrawlerService.doCrawler();
-        if(!resultData.isEmpty()){
+        if(!resultData.isEmpty() && !coolpcFirstLoop ){
             StringBuffer sb = new StringBuffer(coolpcCrawlerService.getStoreTitle());
             sb.append("\n");
             resultData.forEach(item->{
@@ -90,5 +95,6 @@ public class CrawlerJob {
             });
             messager.send(sb.toString());
         }
+        coolpcFirstLoop = false;
     }
 }
--- src/main/java/com/ecommerce/crawler/service/MomoCrawlerService.java
+++ src/main/java/com/ecommerce/crawler/service/MomoCrawlerService.java
@@ -35,6 +35,7 @@ import java.util.stream.Stream;
 
 //https://m.momoshop.com.tw/cateGoods.momo?cn=4302000169&page=1&sortType=5&imgSH=fourCardStyle
 //https://m.momoshop.com.tw/cateGoods.momo?cn=4302000216&page=1&sortType=4&imgSH=fourCardType
+//https://m.momoshop.com.tw/search.momo?searchKeyword=3070&couponSeq=&searchType=2&cateLevel=NaN&curPage=1&cateCode=1200200000&cateName=%E9%9B%BB%E8%85%A6%E7%B5%84%E4%BB%B6%2822%29&maxPage=2&minPage=1&_advCp=N&_advFirst=N&_advFreeze=N&_advSuperstore=N&_advTvShop=N&_advTomorrow=N&_advNAM=N&_advStock=N&_advPrefere=N&_advThreeHours=N&_brandNoList=&ent=b&_imgSH=fourCardType&specialGoodsType=&_isFuzzy=0
 
 @Service
 @Slf4j
@@ -44,11 +45,39 @@ public class MomoCrawlerService extends AbstractCrawlerService implements ICrawl
     final String soldout_keyword = "forso";
     final String mobile_momo_url = "https://m.momoshop.com.tw/";
     final String mobile_momo_goods_url = "https://m.momoshop.com.tw/cateGoods.momo";
-
+    final String mobile_momo_search_url = "https://m.momoshop.com.tw/search.momo";
     @Resource
     RestTemplate restTemplate;
 
-    BiFunction<MultiValueMap<String, String>, ConcurrentMap<String, String>, Void> crawlerAllPage = (uriVars, momoRecorderMap) ->{
+    BiFunction<MultiValueMap<String, String>, ConcurrentMap<String, String>, Void> crawlerSearch = (uriVars, momoRecorderMap) ->{
+        UriComponents uriComponents = UriComponentsBuilder.fromUriString(mobile_momo_search_url).queryParams(uriVars).build();
+        log.info("uri :{}", uriComponents.toUriString());
+        final String url = uriComponents.toUriString();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl(CacheControl.noCache());
+        HttpEntity httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> resp = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+        log.debug("resp body:{}", resp.getBody());
+        Document doc  = Jsoup.parse(resp.getBody());
+        Elements elements = doc.select("li.goodsItemLi");
+
+        for(Element element: elements){
+            if(!element.childNode(1).attr("value").equals("0")){
+                String prodName = element.childNode(9).attr("title");
+                String prodPrice = element.select("b.price").text();
+                String prodUrl  = element.childNode(9).attr("href");
+                String fullInfo = prodPrice +"_"+ prodName;
+                String fullUrl  = mobile_momo_url + prodUrl;
+                log.info("{} {}", fullInfo, fullUrl);
+                momoRecorderMap.put(fullInfo, fullUrl);
+            }
+        }
+        return null;
+    };
+
+    BiFunction<MultiValueMap<String, String>, ConcurrentMap<String, String>, Void> crawlerCateGoods = (uriVars, momoRecorderMap) ->{
         UriComponents uriComponents = UriComponentsBuilder.fromUriString(mobile_momo_goods_url).queryParams(uriVars).build();
         log.info("uri :{}", uriComponents.toUriString());
         final String url = uriComponents.toUriString();
@@ -65,7 +94,7 @@ public class MomoCrawlerService extends AbstractCrawlerService implements ICrawl
         for(Element element: elements){
             if(!element.childNode(7).outerHtml().contains(soldout_keyword)){
                 String prodName = element.attr("title");
-                String prodPrice = element.select("b.price").text();
+                String prodPrice = element.select("b.price").text().replaceAll(",","");
                 String prodUrl  = element.attr("href");
                 String fullInfo = prodPrice +"_"+ prodName;
                 String fullUrl  = mobile_momo_url + prodUrl;
@@ -80,27 +109,72 @@ public class MomoCrawlerService extends AbstractCrawlerService implements ICrawl
     @SneakyThrows
     public void crawler() {
         ConcurrentMap<String, String> momoRecorderMap =  new ConcurrentHashMap<>();
-        for(int page = 1 ; page < 6; page++){
+        for(int page = 1 ; page < 4; page++){
             MultiValueMap<String, String>uriVars = new LinkedMultiValueMap<>();
             uriVars.add("cn","4302000169");
             uriVars.add("page", String.valueOf(page));
             uriVars.add("sortType", "5");
             uriVars.add("imgSH", "fourCardStyle");
-            crawlerAllPage.apply(uriVars, momoRecorderMap);
-            Thread.sleep(200l);
+            crawlerCateGoods.apply(uriVars, momoRecorderMap);
+            Thread.sleep(100l);
             MultiValueMap<String, String>uriVars2 = new LinkedMultiValueMap<>();
             uriVars2.add("cn","4302000216");
             uriVars2.add("page", String.valueOf(page));
             uriVars2.add("sortType", "4");
             uriVars2.add("imgSH", "fourCardStyle");
-            crawlerAllPage.apply(uriVars2, momoRecorderMap);
-            Thread.sleep(200l);
+            crawlerCateGoods.apply(uriVars2, momoRecorderMap);
+            Thread.sleep(100l);
+
+            //search 3060
+            MultiValueMap<String, String> rtx3060map =  getSearchMap(String.valueOf(page), "3060");
+            crawlerSearch.apply(rtx3060map, momoRecorderMap);
+            Thread.sleep(100l);
+
+            //search 3070
+            MultiValueMap<String, String> rtx3070map =  getSearchMap(String.valueOf(page), "3070");
+            crawlerSearch.apply(rtx3070map, momoRecorderMap);
+            Thread.sleep(100l);
+
+            //search 3080
+            MultiValueMap<String, String> rtx3080map =  getSearchMap(String.valueOf(page), "3080");
+            crawlerSearch.apply(rtx3080map, momoRecorderMap);
+            Thread.sleep(100l);
+
+            //search 3090
+            MultiValueMap<String, String> rtx3090map =  getSearchMap(String.valueOf(page), "3090");
+            crawlerSearch.apply(rtx3080map, momoRecorderMap);
+            Thread.sleep(100l);
         }
         log.info("momoRecorderMap size: {}", momoRecorderMap.size());
         crawlerServiceDTO.setMomoCrawlerRecorderMap(momoRecorderMap);
         log.info("momoRecorderMap: {}", momoRecorderMap);
     }
 
+    private MultiValueMap<String, String> getSearchMap(String page, String keyword){
+        MultiValueMap<String, String> varsMap = new LinkedMultiValueMap<>();
+        varsMap.add("searchKeyword", keyword);
+        varsMap.add("searchType", "2");
+        varsMap.add("cateLevel", "NaN");
+        varsMap.add("curPage", page);
+        varsMap.add("cateCode", "1200200000");
+        varsMap.add("cateName", "電腦組件");
+        varsMap.add("maxPage", "2");
+        varsMap.add("minPage", "1");
+        varsMap.add("_advCp", "N");
+        varsMap.add("_advFirst", "N");
+        varsMap.add("_advFreeze", "N");
+        varsMap.add("_advSuperstore", "N");
+        varsMap.add("_advTvShop", "N");
+        varsMap.add("_advTomorrow", "N");
+        varsMap.add("_advNAM", "N");
+        varsMap.add("_advStock", "N");
+        varsMap.add("_advPrefere", "N");
+        varsMap.add("_advThreeHours", "N");
+        varsMap.add("ent", "b");
+        varsMap.add("_imgSH", "fourCardType");
+        varsMap.add("_isFuzzy", "0");
+        return varsMap;
+    }
     @Override
     public void parse() {
         Map<String, String>  recorderMap = crawlerServiceDTO.getMomoCrawlerRecorderMap();
